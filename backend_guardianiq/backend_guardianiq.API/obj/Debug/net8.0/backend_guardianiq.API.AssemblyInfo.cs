e378caa278c5c5b29a1a637cd91efaeec36dbdd5
Astrid Boronda Heidinger <108700549+AstridBH@users.noreply.github.com>
2024-06-27 23:42:36 -0500

    Init

--- backend_guardianiq/backend_guardianiq.API/obj/Debug/net8.0/backend_guardianiq.API.AssemblyInfo.cs
+++ backend_guardianiq/backend_guardianiq.API/obj/Debug/net8.0/backend_guardianiq.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend_guardianiq.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa7356a562925004fcfaa8bf55097ad9a90140ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend_guardianiq.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend_guardianiq.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
